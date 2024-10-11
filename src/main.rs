332dcf85cd272b58cfee983b1a79875b03a53759
Dramalf <dramalfcsc@gmail.com>
2024-10-11 00:44:58 -0700

    test:EB4

--- src/main.rs
+++ src/main.rs
@@ -6,17 +6,33 @@ use std::env;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
-        // Example: "5:hello" -> "hello"
-        let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
-        return serde_json::Value::String(string.to_string());
-    } else {
-        panic!("Unhandled encoded value: {}", encoded_value)
+    match encoded_value.chars().next().unwrap() {
+        c if c.is_digit(10) => {
+            // Example: "5:hello" -> "hello"
+            let colon_index = encoded_value.find(':').unwrap();
+            let number_string = &encoded_value[..colon_index];
+            let number = number_string.parse::<i64>().unwrap();
+            let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+            serde_json::Value::String(string.to_string())
+        },
+        'i' => {
+            let end_index = encoded_value.find('e').unwrap();
+            let number_string:&str=&encoded_value[1..end_index];
+            let number:i64=number_string.parse::<i64>().unwrap();
+            let real_number_str=number.to_string();
+            if real_number_str.len()==number_string.len(){
+                // serde_json::Value::Number(serde_json::Number::from(number))
+                number.into()
+            }
+            else{
+                panic!("Unhandled encoded value: {}", encoded_value)
+            } 
+        },
+        _ => {
+            panic!("Unhandled encoded value: {}", encoded_value)
+        }
     }
+
 }
 
 // Usage: your_bittorrent.sh decode "<encoded_value>"
